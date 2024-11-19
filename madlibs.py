b3288bf6dfdc4546d843abc771d80c2016225742
Mick <the.m1ck@proton.me>
2024-11-19 08:59:31 -0700

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+
+print ('Welcome! You are about to play Mad Libs! I will ask you for nouns, verbs, adjectives, proper nouns and adverbs.' + '\n' + 'Using those words I will create an  unexpected story for you!')
+pronoun1=input('Please pick a pronoun. ')
+nounlocation1=input('Please pick a location. ')
+nounlocation2=input('Please pick a second location. ')
+pluralnoun1=input('Please pick a plural noun. ')
+singularnoun1=input('Please pick a singular noun. ')
+pasttenseverb1=input('Please pick a past tense verb. ')
+adverb1=input('Please pick an adverb. ')
+adjective=input('Please pick an adjective. ')
+print ('One fine morning' + ' ' + pronoun1 + ' ' + 'went to the' + ' ' + nounlocation1 + ' ' + 'to buy a handfull of ' + pluralnoun1 + '.' + '\n' + 'As' + ' ' + pronoun1 + ' ' + pasttenseverb1 + ' ' + adverb1 + ' back to his/her ' + nounlocation2 + ' ' + 'they noticed that his/her ' + pluralnoun1 + ' ' + 'were no longer in their pocket.' + '\n' + 'It turned out that on the way to the' + ' ' + nounlocation2 + ' ' + 'a' + ' ' + adjective + ' ' + singularnoun1 + ' took the ' + pluralnoun1 + ' ' + 'never to be seen again.')
